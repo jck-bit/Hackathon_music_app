ca0035911847f1794e65f029e6982d092b63e97d
jck-bit <jckkinyanjui@gmail.com>
2024-09-23 12:00:56 +0300

    edited dockerfile

--- src/components/Digit/index.tsx
+++ src/components/Digit/index.tsx
@@ -1,14 +1,14 @@
-import './styles.scss';
+import "./styles.scss";
 
 const Digit = ({ value, title }: any) => {
-  const leftDigit = value >= 10 ? value.toString()[0] : '0';
+  const leftDigit = value >= 10 ? value.toString()[0] : "0";
   const rightDigit = value >= 10 ? value.toString()[1] : value.toString();
   return (
-    <div className='containerWrap'>
-      <span className='title'>{title}</span>
-      <div className='digitContainer'>
-        <span className='singleDigit'>{leftDigit}</span>
-        <span className='singleDigit'>{rightDigit}</span>
+    <div className="containerWrap">
+      <span className="title">{title}</span>
+      <div className="digitContainer">
+        <span className="singleDigit">{leftDigit}</span>
+        <span className="singleDigit">{rightDigit}</span>
       </div>
     </div>
   );
